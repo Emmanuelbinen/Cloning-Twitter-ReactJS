7d33fb17a392ffad8fa76350e83f29b06e475360
Emmanuel Binen <emmanuelbinen@outlook.com>
2024-02-25 15:04:03 +0200

    Adding the main component Timeline and header component

---
+++ src/component/mainComponents/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import HeaderRightImage from "../../images/Top-Right-Header-Image.png";
+
+function PageTitle() {
+  return <div className="page-title">Home</div>;
+}
+function TopTweets() {
+  return (
+    <div className="top-tweets">
+      <img src={HeaderRightImage} />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <PageTitle />
+      <TopTweets />
+    </div>
+  );
+}
+export default Header;
---
+++ src/component/mainComponents/Timeline.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "./Header";
+
+export class Timeline extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
